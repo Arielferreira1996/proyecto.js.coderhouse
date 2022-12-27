95f39bdb7c6eecb80800ea7f86e41a163973560f
ariel ferreira <ferreiraariel35@gmail.com>
2022-12-26 21:29:42 -0300

    primer comit

---
+++ principal.js
@@ -0,0 +1,616 @@
+// ARRAY DE PRODUCTOS
+
+const productos = [
+    // PRODUCTOS AMD PROCESADORES******************************************************************
+    {
+        id:"Ryzen 3",
+        titulo:"Ryzen 3 1200",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_3_1200.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 3",
+        titulo:"Ryzen 3 1300x",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_3_1300x.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 3",
+        titulo:"Ryzen 3 3300x",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_3_3300x.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 5",
+        titulo:"Ryzen 5 1400",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_5_1400.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 5",
+        titulo:"Ryzen 5 1500x",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_5_1500x.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 5",
+        titulo:"Ryzen 5 1600",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_5_1600.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 5",
+        titulo:"Ryzen 5 1600AF",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_5_1600_AF.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 5",
+        titulo:"Ryzen 5 1600X",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_5_1600X.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 5",
+        titulo:"Ryzen 5 2600X",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_5_2600X.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 5",
+        titulo:"Ryzen 5 3600",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_5_3600.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 5",
+        titulo:"Ryzen 5 3600X",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_5_3600X.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 5",
+        titulo:"Ryzen 5 3600XT",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_5_3600XT.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 7",
+        titulo:"Ryzen 5 1700",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_7_1700.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 7",
+        titulo:"Ryzen 7 1700X",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_7_1700X.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 7",
+        titulo:"Ryzen 7 1800X",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_7_1800X.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 7",
+        titulo:"Ryzen 7 2700X",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_7_2700X.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 7",
+        titulo:"Ryzen 7 3700X",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_7_3700X.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 7",
+        titulo:"Ryzen 7 3800X",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_7_3800X.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 7",
+        titulo:"Ryzen 7 3800XT",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_7_3800XT.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 9",
+        titulo:"Ryzen 9 3900X",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_9_3900X.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Ryzen 9 ",
+        titulo:"Ryzen 9 3900XT",
+        imagen:"./imagenes de proyecto js/amd/Ryzen_9_3900XT.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+
+    // PRODUCTOS AMD MOTHERBOARD*************************************************************************
+    {
+        id:"Motherboard AMD",
+        titulo:"Mother TUF_X470-Plus_Gaming",
+        imagen:"./imagenes de proyecto js/amd/TUF_X470-Plus_Gaming.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard AMD",
+        titulo:"Mother Prime_A520M-A",
+        imagen:"./imagenes de proyecto js/amd/Prime_A520M-A.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard AMD",
+        titulo:"Mother Prime_B350-Plus",
+        imagen:"./imagenes de proyecto js/amd/Prime_B350-Plus.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard AMD",
+        titulo:"Mother Prime_B550M-K",
+        imagen:"./imagenes de proyecto js/amd/Prime_B550M-K.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard AMD",
+        titulo:"Mother Prime_X470-Pro",
+        imagen:"./imagenes de proyecto js/amd/Prime_X470-Pro.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard AMD",
+        titulo:"Mother TUF_X470-Plus_Gaming",
+        imagen:"./imagenes de proyecto js/amd/TUF_X470-Plus_Gaming.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard AMD",
+        titulo:"Mother Prime_X570-Pro",
+        imagen:"./imagenes de proyecto js/amd/Prime_X570-Pro.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard AMD",
+        titulo:"Mother ROG_Crosshair_VIII_Hero",
+        imagen:"./imagenes de proyecto js/amd/ROG_Crosshair_VIII_Hero.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard AMD",
+        titulo:"Mother ROG_Strix_x570-F_Gaming",
+        imagen:"./imagenes de proyecto js/amd/ROG_Strix_x570-F_Gaming.jpg",
+        categoria: {
+            nombre:"Amd",
+            id: "Amd",
+        },
+        precio: 1000,
+    },
+    // PRODUCTOS INTEL PROCESADORES********************************************************************
+    {
+        id:"Intel Core i3",
+        titulo:"Intel Core i3-7350k",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i3-7350k.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i3",
+        titulo:"Intel Core i3-8350k",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i3-8350k.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i3",
+        titulo:"Intel Core i3-9350kF",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i3-9350kF.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i5",
+        titulo:"Intel Core i5-7640X",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i5-7640X.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i5",
+        titulo:"Intel Core i5-8600k",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i5-8600k.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i5",
+        titulo:"Intel Core i5-9600k",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i5-9600k.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i7",
+        titulo:"Intel Core i7-7700",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i7-7700.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i7",
+        titulo:"Intel Core i7-8700k",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i7-8700k.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i7",
+        titulo:"Intel Core i7-9700k",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i7-9700k.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i7",
+        titulo:"Intel Core i7-10700k",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i7-10700k.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i9",
+        titulo:"Intel Core i9-7980XE",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i9-7980XE.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Intel Core i9",
+        titulo:"Intel Core i9-9900k",
+        imagen:"./imagenes de proyecto js/intel/Intel_Core_i9-9900k.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    // PRODUCTOS INTEL MOTHERBOARD***************************************************************************
+    {
+        id:"Motherboard",
+        titulo:"ASUS Prime x299-A_II",
+        imagen:"./imagenes de proyecto js/intel/ASUS_Prime_x299-A_II.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard",
+        titulo:"ASUS Prime Z390M-PLUS",
+        imagen:"./imagenes de proyecto js/intel/ASUS_Prime_Z390M-PLUS.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard",
+        titulo:"ASUS ROG Maximus X Formula",
+        imagen:"./imagenes de proyecto js/intel/ASUS_ROG_Maximus_X_Formula.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard",
+        titulo:"ASUS ROG MAXIMUS XI HERO",
+        imagen:"./imagenes de proyecto js/intel/ASUS_ROG_MAXIMUS_XI_HERO.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard",
+        titulo:"ASUS ROG Rampage VI Extreme encore",
+        imagen:"./imagenes de proyecto js/intel/ASUS_ROG_Rampage_VI_Extreme_encore.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard",
+        titulo:"ASUS ROG STRIX Z370-I GAMING",
+        imagen:"./imagenes de proyecto js/intel/ASUS_ROG_STRIX_Z370-I_GAMING.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard",
+        titulo:"ASUS ROG STRIX H370-I GAMING",
+        imagen:"./imagenes de proyecto js/intel/ASUS_ROG_STRIX_H370-I_GAMING.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Motherboard",
+        titulo:"ASUS ROG STRIX Z390-E GAMING",
+        imagen:"./imagenes de proyecto js/intel/ASUS_ROG_STRIX_Z390-E_GAMING.jpg",
+        categoria: {
+            nombre:"Intel",
+            id: "Intel",
+        },
+        precio: 1000,
+    },
+
+    // PRODUCTOS TARJETAS DE VIDEO*************************************************************
+    {
+        id:"Tarjeta de Video",
+        titulo:"AMD Radeon RX 550",
+        imagen:"./imagenes de proyecto js/tarjeta de video/AMD_Radeon_RX_550.jpg",
+        categoria: {
+            nombre:"AMD",
+            id: "AMD",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Tarjeta de Video",
+        titulo:"ASUS ROG STRIX 1660 TI",
+        imagen:"./imagenes de proyecto js/tarjeta de video/ASUS ROG STRIX 1660 TI.jpg",
+        categoria: {
+            nombre:"tarjeta de video",
+            id: "tarjeta de video",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Tarjeta de Video",
+        titulo:"AMD Radeon RX 570 RS",
+        imagen:"./imagenes de proyecto js/tarjeta de video/AMD_Radeon_RX_570_RS.jpg",
+        categoria: {
+            nombre:"AMD",
+            id: "AMD",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Tarjeta de Video",
+        titulo:"AMD Radeon RX 6500 XT",
+        imagen:"./imagenes de proyecto js/tarjeta de video/AMD_Radeon_RX_6500XT.jpg",
+        categoria: {
+            nombre:"AMD",
+            id: "AMD",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Tarjeta de Video",
+        titulo:"MSI GeForce GTX 1650 Super",
+        imagen:"./imagenes de proyecto js/tarjeta de video/MSI_GeForce_GTX_1650_Super_Gaming_X.jpg",
+        categoria: {
+            nombre:"AMD",
+            id: "AMD",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Tarjeta de Video",
+        titulo:"MSI GeForce RTX 2070 Super",
+        imagen:"./imagenes de proyecto js/tarjeta de video/MSI_GeForce_RTX_2070_SUPER.jpg",
+        categoria: {
+            nombre:"AMD",
+            id: "AMD",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Tarjeta de Video",
+        titulo:"MSI GeForce RTX 3080",
+        imagen:"./imagenes de proyecto js/tarjeta de video/MSI_GeForce_RTX_3080_GAMING_X_TRIO .jpg",
+        categoria: {
+            nombre:"AMD",
+            id: "AMD",
+        },
+        precio: 1000,
+    },
+    {
+        id:"Tarjeta de Video",
+        titulo:"XFX AMD Radeon RX 5700 XT",
+        imagen:"./imagenes de proyecto js/tarjeta de video/XFX_AMD_Radeon_RX_5700_XT.jpg",
+        categoria: {
+            nombre:"AMD",
+            id: "AMD",
+        },
+        precio: 1000,
+    },
+
+]
+
+// DOCUMENTOS TRAIDOS DEL DOM PARA TRABAJARLOS EN JS*****************************************************
+
+const contenedorProductos = document.querySelector(".contenedor-productos")
+
+// FUNCION PARA CARGAR PRODUCTOS DEL ARRAY***************************************************
+
+function pintarProductos() {
+    productos.forEach(producto =>{
+        let div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+        <img class="prod-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+        <div class="prod-detalles">
+            <h3 class="prod-titulo">${producto.titulo}</h3>
+            <p class="prod-precio">${producto.precio}</p>
+            <button class="prod-agregar" id="${producto.id}">Agregar</button>
+        </div>
+        `;
+        contenedorProductos.append(div);
+})
+}
+
+pintarProductos();
